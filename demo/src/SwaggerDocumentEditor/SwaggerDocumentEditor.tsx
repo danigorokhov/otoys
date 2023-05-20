f1d063c67c788ed4dbc2eda0d9255f2c55494ce5
Daniil Gorokhov <kripa0cool@mail.ru>
2023-05-20 15:33:10 +0300

    feat: added state management to swagger document editor

--- demo/src/SwaggerDocumentEditor/SwaggerDocumentEditor.tsx
+++ demo/src/SwaggerDocumentEditor/SwaggerDocumentEditor.tsx
@@ -1,24 +1,45 @@
-import React, {FC, useCallback} from 'react';
+import React, {FC, useCallback, useEffect} from 'react';
 import { SwaggerDocumentEditorProps, HandleChange } from './SwaggerDocumentEditor.types';
 import {cn} from './SwaggerDocumentEditor.cn';
-import DEFAULT_VALUE from './SwaggerDocumentEditor.assets/petstore3.json';
 import { CodeEditor } from '../CodeEditor';
 import './SwaggerDocumentEditor.css';
+import SWAGGER_DOCUMENT_DEFAULT_VALUE from './SwaggerDocumentEditor.assets/petstore3.json';
+
+import {useUnit} from 'effector-react';
+import './SwaggerDocumentEditor.models/init';
+import { $swaggerDocument, swaggerDocumentInitialized, swaggerDocumentChanged, $isSwaggerDocumentInitialized } from './SwaggerDocumentEditor.models';
 
 export const SwaggerDocumentEditor: FC<SwaggerDocumentEditorProps> = props => {
     const {
         className,
     } = props;
 
-    const handleChange = useCallback<HandleChange>(_value => {
-        // TODO handle _value
-    }, []);
+    const [
+        swaggerDocument,
+        setSwaggerDocument,
+    ] = useUnit([$swaggerDocument, swaggerDocumentChanged]);
+
+    const handleChange = useCallback<HandleChange>(value => {
+        setSwaggerDocument(value || '');
+    }, [setSwaggerDocument]);
+
+    const [
+        isSwaggerDocumentInitialized,
+        setSwaggerDocumentInitialized,
+    ] = useUnit([$isSwaggerDocumentInitialized, swaggerDocumentInitialized]);
+
+    useEffect(() => {
+        if (isSwaggerDocumentInitialized) return;
+
+        setSwaggerDocument(JSON.stringify(SWAGGER_DOCUMENT_DEFAULT_VALUE, null, 4));
+        setSwaggerDocumentInitialized(true);
+    }, [isSwaggerDocumentInitialized, setSwaggerDocument, setSwaggerDocumentInitialized]);
 
     return (
         <CodeEditor
             className={cn(null, [className])}
             language="json" // TODO support yaml
-            defaultValue={JSON.stringify(DEFAULT_VALUE, null, 4)} // TODO support document loading
+            defaultValue={swaggerDocument}
             onChange={handleChange}
             path="swaggerDocument.json"
         />
