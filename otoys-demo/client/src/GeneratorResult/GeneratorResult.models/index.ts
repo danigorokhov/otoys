3907c8c8d33453a0546379b91e177247c65e46af
Daniil Gorokhov <kripa0cool@mail.ru>
2023-05-22 17:53:37 +0300

    feat: added type generation via server

--- otoys-demo/client/src/GeneratorResult/GeneratorResult.models/index.ts
+++ otoys-demo/client/src/GeneratorResult/GeneratorResult.models/index.ts
@@ -13,18 +13,21 @@ type LoadGeneratorResultParams = {
 type LoadGeneratorResultFx = (params: LoadGeneratorResultParams) => Promise<string>;
 
 type ApiGenerateResponse = { // TODO to generated types
-    code: string;
+    result: string;
 };
 
 export const loadGeneratorResultFx = createEffect<LoadGeneratorResultFx>(async params => {
     const response = await fetch('http://localhost:3030/api/generate', { // TODO to config.server.origin
         method: 'POST',
         body: JSON.stringify(params),
+        headers: {
+            'Content-Type': 'application/json',
+        },
     });
 
-    const result = await response.json() as ApiGenerateResponse;
+    const { result } = await response.json() as ApiGenerateResponse;
 
-    return result.code;
+    return result;
 });
 
 export const $generatorResultStatus = status({
--- otoys-demo/server/src/modules/api/index.ts
+++ otoys-demo/server/src/modules/api/index.ts
@@ -1,9 +1,48 @@
 import { FastifyPluginAsync } from "fastify";
+import { generateApi } from 'swagger-typescript-api';
 
 export const api: FastifyPluginAsync = async (fastify) => {
-    fastify.post('/api/generate', (_request, reply) => {
-        reply
-            .code(200)
-            .send({ code: `type Hello = 'world!';\n/* ${Date.now()} */` });
-    });
+    fastify.post(
+        '/api/generate',
+        {
+            schema: {
+                body: {
+                    type: 'object',
+                    properties: {
+                        document: {
+                            type: 'string',
+                        },
+                        generatorSettings: {
+                            type: 'object',
+                            properties: {
+                                pathWhitelist: {
+                                    type: 'string',
+                                },
+                            },
+                            required: [
+                                'pathWhitelist',
+                            ],
+                        },
+                    },
+                    required: [
+                        'document',
+                    ],
+                },
+            },
+        },
+        async (_request, reply) => {
+            const body = _request.body as { document: string, generatorSettings: { pathWhitelist?: string } }; // TODO common type
+
+            const generatedCode = await generateApi({
+                spec: JSON.parse(body.document),
+                generateClient: false,
+            });
+
+            const { content: result } = generatedCode.files[0];
+
+            reply
+                .code(200)
+                .send({ result });
+        },
+    );
 };
