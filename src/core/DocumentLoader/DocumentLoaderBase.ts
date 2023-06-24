51b90e2be908a8fffc559aedf21c8010ee29b7e7
Daniil Gorokhov <oneleques@yandex.ru>
2023-06-24 12:21:23 +0300

    feat: added validation of openapi document version

--- src/core/DocumentLoader/DocumentLoaderBase.ts
+++ src/core/DocumentLoader/DocumentLoaderBase.ts
@@ -6,8 +6,18 @@ export type DocumentType = 'yaml' | 'json';
 
 export abstract class DocumentLoaderBase {
     static validate(document: unknown) {
-        // TODO support validation
-        return document as OpenAPIObject;
+        if (
+            typeof document === 'object' && document !== null &&
+            'openapi' in document && typeof document.openapi === 'string' &&
+            document.openapi.startsWith('3.0')
+        ) {
+            // TODO support validation
+            return document as OpenAPIObject;
+        }
+
+        throw new Error(
+            'DocumentLoaderBase: document validation failed. Supported only OpenAPI v3.0',
+        );
     }
 
     static parse(document: string, type: DocumentType) {
