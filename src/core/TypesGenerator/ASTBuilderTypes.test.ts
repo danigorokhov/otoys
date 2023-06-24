ab42f6d6e1bfbd32a0eaec56b2e5b7f32f2166ff
Daniil Gorokhov <oneleques@yandex.ru>
2023-06-24 12:18:40 +0300

    feat: added ASTBuilderTypes

---
+++ src/core/TypesGenerator/ASTBuilderTypes.test.ts
@@ -0,0 +1,25 @@
+import { expect, describe, it } from '@jest/globals';
+import { OpenAPIObject } from 'openapi3-ts/oas30';
+
+import { ASTBuilderTypes } from './ASTBuilderTypes';
+import { SchemaCollector } from './SchemaCollector';
+import { RefResolver } from './RefResolver';
+import { PathCollector } from './PathCollector';
+
+import petstore3 from '../../../examples/v3/petstore3.json';
+
+describe('TypesGenerator/TypesGeneratorV3/ASTBuilderTypes', () => {
+    // TODO more detailed tests
+    it('should build AST', () => {
+        const document = petstore3 as unknown as OpenAPIObject;
+        const resolver = new RefResolver(document);
+        const pathCollector = new PathCollector(document.paths, undefined);
+        const paths = pathCollector.collect();
+        const schemaCollector = new SchemaCollector(paths, resolver);
+        const schemas = schemaCollector.collect();
+        const builder = new ASTBuilderTypes(schemas, resolver);
+
+        expect(builder.build()).toMatchSnapshot();
+    });
+});
+
