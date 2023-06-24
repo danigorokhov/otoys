c976312df32ffacfa00914d8faad3f8bb10fd5d4
Daniil Gorokhov <oneleques@yandex.ru>
2023-06-24 22:35:53 +0300

    feat: added theme switching

--- otoys-demo/client/src/Header/Header.tsx
+++ otoys-demo/client/src/Header/Header.tsx
@@ -1,22 +1,59 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect} from 'react';
 import { HeaderProps } from './Header.types';
 import {cn} from './Header.cn';
 import './Header.css';
 import {Logo} from '../Logo';
 import {LangSwitch} from '../LangSwitch';
+import {ThemeSwitch} from '../ThemeSwitch';
 import {ReactComponent as ExternalLinkIcon} from './Header.assets/external-link.svg';
 import { Link, Text } from '@gravity-ui/uikit';
+
+let yIdInitialized = false;
 
 export const Header: FC<HeaderProps> = props => {
     const {
         className,
     } = props;
 
+    useEffect(() => {
+        if (yIdInitialized) return;
+
+        // @ts-ignore
+        window.YaAuthSuggest.init({
+            client_id: '820a260202514b2db22ce8840cf0ee31',
+            response_type: 'token',
+            redirect_uri: 'https://local.otoys.tech:3000/suggest/token',
+        },
+        'https://local.otoys.tech:3000',
+        {
+            view: 'button',
+            parentId: 'yIdContainer',
+            buttonView: 'main',
+            buttonTheme: 'dark',
+            buttonSize: 'xs',
+            buttonBorderRadius: 6,
+        },
+        )
+            .then(({
+                // @ts-ignore
+                handler,
+            }) => {
+                return handler();
+            })
+            // @ts-ignore
+            .then(data => console.log('Сообщение с токеном', data))
+            // @ts-ignore
+            .catch(error => console.log('Обработка ошибки', error));
+
+        yIdInitialized = true;
+    }, []);
+
     return (
         <header className={cn(null, [className])}>
             <Logo />
 
             <LangSwitch />
+            <ThemeSwitch />
             <Link
                 className={cn('ExternalLink')}
                 view="primary"
@@ -27,7 +64,6 @@ export const Header: FC<HeaderProps> = props => {
             </Link>
 
             {/* TODO
-            <ThemeSwitch />
             <UserId />
             */}
         </header>
